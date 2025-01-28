ead05d6134ef77db2eb1d03f9c1752fe743807dc
eugenr <eugenr79@gmail.com>
2025-01-28 17:17:49 +0300

    corrected cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api')
   app.use(cookieParser())
   app.enableCors({
-    origin: ['http://localhost:3000', 'http:/109.71.246.22:3000'],
+    origin: ['http://localhost:3000', 'http:/109.71.246.22', 'http:/109.71.246.22:3000'],
     credentials: true,
     exposedHeaders: 'set-cookie'
   })
